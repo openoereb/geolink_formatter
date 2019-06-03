804c00f0e7044f581cb7a60b05b5dda1294ab8d5
Karsten Deininger <karsten.deininger@bl.ch>
2019-06-03 08:41:58 +0200

    Remove Python 3.7

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(name='geolink_formatter',
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
           "Topic :: Scientific/Engineering :: GIS",
           "Topic :: Software Development :: Libraries :: Python Modules"
       ],
