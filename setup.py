24720462637c6e5c1ef325dd9a3ce63ec979f994
Michael Mueller <michael.mueller@bl.ch>
2024-12-13 12:22:58 +0100

    version='2.0.6'

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
 ]
 
 setup(name='geolink_formatter',
-      version='2.0.5',
+      version='2.0.6',
       description='OEREBlex geoLink Formatter',
       license='BSD',
       long_description='{readme}\n\n{changelog}'.format(readme=readme, changelog=changelog),
